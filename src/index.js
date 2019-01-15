821c01abae0f507c80408b62f300b5ec1df60451
bhayek12@gmail.com <bhayek12@gmail.com>
2019-01-15 16:25:25 -0700

    change file path

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import './index.css';
-import App from './components/App';
+import App from './components/App/index.js';
 import * as serviceWorker from './serviceWorker';
 
 
